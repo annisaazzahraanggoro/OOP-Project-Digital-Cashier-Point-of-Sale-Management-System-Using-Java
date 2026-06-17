4c9b954d94e457236c3dd3154c7c419e3ef3bb23
Annisa Azzahra Anggoro <annisaazzahraanggoro@gmail.com>
2026-06-17 11:32:36 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/kasirdigital/KasirDigital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kasirdigital;
+
+/**
+ *
+ * @author IDEAPAD
+ */
+public class KasirDigital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/kasirdigital/KoneksiDB.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kasirdigital;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class KoneksiDB {
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/db_kasir";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Koneksi Database Berhasil!");
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Koneksi Database Gagal: " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
